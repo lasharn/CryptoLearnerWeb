c373947f21307c3fadbf54b424655e10b863ca38
Lasharn Weerasinghe <lasharn@windowslive.com>
2016-04-08 01:52:37 +1200

    Cleaned up unused default src files

--- CryptoLearnerWeb/src/cryptolearner/client/CryptoLearnerWeb.java
+++ CryptoLearnerWeb/src/cryptolearner/client/CryptoLearnerWeb.java
@@ -1,56 +1,23 @@
 package cryptolearner.client;
 
-import java.awt.Color;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.gwtbootstrap3.client.shared.event.CarouselSlidEvent;
 import org.gwtbootstrap3.client.shared.event.CarouselSlidHandler;
-import org.gwtbootstrap3.client.shared.event.CarouselSlideEvent;
-import org.gwtbootstrap3.client.shared.event.CarouselSlideHandler;
 import org.gwtbootstrap3.client.ui.Button;
 import org.gwtbootstrap3.client.ui.Carousel;
 import org.gwtbootstrap3.client.ui.CarouselControl;
-import org.gwtbootstrap3.client.ui.CarouselIndicators;
 import org.gwtbootstrap3.client.ui.CarouselInner;
 import org.gwtbootstrap3.client.ui.CarouselSlide;
 import org.gwtbootstrap3.client.ui.PageHeader;
 import org.gwtbootstrap3.client.ui.constants.IconType;
 import org.gwtbootstrap3.client.ui.html.Div;
 
-import cryptolearner.shared.FieldVerifier;
-
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.HasHorizontalAlignment;
-import com.google.gwt.user.client.ui.Image;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.user.client.ui.Widget;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class CryptoLearnerWeb implements EntryPoint {
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network "
-			+ "connection and try again.";
-
 	/**
 	 * This is the entry point method.
 	 */
