041fd92a135e1b7dbb767579135d3ba2eaeb1820
Lasharn Weerasinghe <lasharn@windowslive.com>
2016-04-29 14:00:49 +1200

    Carousel menu now navigates to new pages (which aren't implemented yet)

--- app.js
+++ app.js
@@ -1,53 +1,69 @@
 (function () {
     var app = angular.module('cryptoLearner', ['ui.bootstrap']);
-    app.controller('MenuController', function($scope) {
+    app.controller('MenuController', ['$scope', '$window', function($scope, $window) {
         $scope.noWrapSlides = true;
         this.levels = levels;
-    });
+        $scope.go = function (path) {
+          $window.location.href = path;
+        };
+    }]);
     
     var levels = [{
         name: 'Caesar Cipher',
+        level: 'caesar',
         buttons: [{
             name: 'Encrypt',
             isLocked: false,
-            icon: 'glyphicon-pencil'
+            icon: 'glyphicon-pencil',
+            challenge: 1
         }, {
             name: '',
             isLocked: true,
-            icon: ''
+            icon: '',
+            challenge: 2
         }, {
             name: '',
             isLocked: true,
+            icon: '',
+            challenge: 3
         }]
     }, {
         name: 'Vigenere Cipher',
+        level: 'vigenere',
         buttons: [{
             name: 'Encrypt',
             isLocked: false,
-            icon: 'glyphicon-pencil'
+            icon: 'glyphicon-pencil',
+            challenge: 1
         }, {
             name: '',
             isLocked: true,
-            icon: ''
+            icon: '',
+            challenge: 2
         }, {
             name: '',
             isLocked: true,
-            icon: ''
+            icon: '',
+            challenge: 3
         }]
     }, {
         name: 'Substitution Cipher',
+        level: 'substitution',
         buttons: [{
             name: 'Encrypt',
             isLocked: false,
-            icon: 'glyphicon-pencil'
+            icon: 'glyphicon-pencil',
+            challenge: 1
         }, {
             name: '',
             isLocked: true,
-            icon: ''
+            icon: '',
+            challenge: 2
         }, {
             name: '',
             isLocked: true,
-            icon: ''
+            icon: '',
+            challenge: 3
         }]
     }];
         
