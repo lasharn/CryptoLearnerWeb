bdb002bab69d46e790441c6f0638e3c68f484b28
Lasharn Weerasinghe <lasharn@windowslive.com>
2016-05-27 10:17:12 +1200

    Caesar cipher level retrieves a random word from a list and picks a random key.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,52 @@
+
+
 (function () {
     var app = angular.module('cryptoLearner', ['ui.bootstrap']);
     app.controller('MenuController', ['$scope', '$window', function($scope, $window) {
         $scope.noWrapSlides = true;
         this.levels = levels;
         $scope.go = function (path) {
-          $window.location.href = path;
+            $window.location.href = path;
         };
+    }]);
+    
+    app.controller('GameController', ['$scope', '$window', function($scope, $window) {
+        this.state = levelState;
+    }]);
+    
+    app.controller('CaesarLevelOne', ['$scope', '$window', function($scope, $window) {
+        this.wordList = getWordList();
+        $scope.key = Math.floor((Math.random() * 25) + 1);
+        $scope.word = newWord();
+        $scope.setupChallenge = function() {
+            $scope.key = Math.floor((Math.random() * 25) + 1);
+            $scope.word = newWord();
+        }
+        
+        function getWordList() {
+            // read the list of words in the words.txt file
+            var rawFile = new XMLHttpRequest();
+            rawFile.open("GET", "../res/words.txt", false);
+            rawFile.onreadystatechange = function ()
+            {
+                if(rawFile.readyState === 4)
+                {
+                    if(rawFile.status === 200 || rawFile.status == 0)
+                    {
+                        var allText = rawFile.responseText;
+                        wordList = allText.split("\n");
+                    }
+                }
+            }
+            rawFile.send(null);
+            return wordList;
+        }
+        
+        function newWord() {
+            // retrieve a random word from the list of words
+            var index = Math.floor(Math.random() * this.wordList.length);
+            return this.wordList[index];
+        }
     }]);
     
     var levels = [{
@@ -66,5 +107,10 @@
             challenge: 3
         }]
     }];
+    
+    var levelState = {
+        currentStage: 0,
+        maxStage: 3
+    };                             
         
 })();
