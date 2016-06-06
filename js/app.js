d8f6fd92cef399e83455952674593828f36c62c9
Lasharn Weerasinghe <lasharn@windowslive.com>
2016-06-06 15:17:24 +1200

    Letters for the on screen keyboard are calculated.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-
-
 (function () {
     var app = angular.module('cryptoLearner', ['ui.bootstrap']);
     app.controller('MenuController', ['$scope', '$window', function($scope, $window) {
@@ -19,12 +17,16 @@
         $scope.key = Math.floor((Math.random() * 25) + 1);
         $scope.word = newWord();
         $scope.answer = encryptCaesar($scope.word, $scope.key);
+        $scope.keyboard = createKeyboard($scope.answer);
         $scope.setupChallenge = function() {
             $scope.key = Math.floor((Math.random() * 25) + 1);
             $scope.word = newWord();
             $scope.answer = encryptCaesar($scope.word, $scope.key);
+            $scope.keyboard = createKeyboard($scope.answer);
             $scope.$apply();
         }
+        
+        
         
         function getWordList() {
             // read the list of words in the words.txt file
@@ -50,6 +52,41 @@
             var index = Math.floor(Math.random() * this.wordList.length);
             return this.wordList[index].toUpperCase();
         }
+
+        function shuffle(str) {
+            var a = str.split(""),
+                n = a.length;
+
+            for(var i = n - 1; i > 0; i--) {
+                var j = Math.floor(Math.random() * (i + 1));
+                var tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+            }
+            return a.join("");
+        }
+        
+        function createKeyboard(encryptedWord) {
+            var KEYBOARD_SIZE = 10;
+            var word = encryptedWord;
+            var uniqueStr = "";
+            for (i = 0; i < word.length; i++) {
+                if (uniqueStr.indexOf(word.charAt(i)) == -1) {
+                    uniqueStr += word[i];    
+                }
+            }
+            // pad the keyboard with random letters
+            while (uniqueStr.length < KEYBOARD_SIZE + 1) {
+                var newLetter = String.fromCharCode(Math.floor((Math.random() * 26)) + "A".charCodeAt(0));
+                if (uniqueStr.indexOf(newLetter) == -1) {
+                    uniqueStr += newLetter;    
+                }
+            }
+            console.log(uniqueStr);
+            uniqueStr = shuffle(uniqueStr);
+            console.log(uniqueStr);
+            return uniqueStr;
+        }
         
         function encryptCaesar(plainText, key) {
             plainText = plainText.toUpperCase();
@@ -62,6 +99,8 @@
                 cipherTextArray[i] = String.fromCharCode(newValue);
             }
             cipherText = cipherTextArray.join("");
+            // remove non-alphabetical characters
+            cipherText = cipherText.replace(/\W+/g, "");
             return cipherText;    
         }
     }]);
