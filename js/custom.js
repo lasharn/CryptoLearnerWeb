d7107c2cb9565eb1cebb109906daf316086309c3
Lasharn Weerasinghe <lasharn@windowslive.com>
2016-08-19 15:05:21 +1200

    Added shared layout pages for nav bar

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+/**
+ * Created by lwwe379 on 19/08/2016.
+ */
+
+//include shared layout items
+$(function(){
+    //navigation bar
+    $("#nav").load("/shared/nav.html");
+    $("#home-nav").load("/shared/home-nav.html");
+});
